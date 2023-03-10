c212f4eff9211feb6615d9ddc87858a4336a34ca
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-03-10 10:28:24 +0100

    test files

---
+++ generateSpecs.js
@@ -0,0 +1,34 @@
+const Handlebars = require("handlebars");
+const fs = require("fs");
+
+const elmSpec = `module Tests{{i}} exposing (..)
+
+import Expect
+import Sum exposing (sum)
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "sum"
+        [ test "1 + 2 = 3" <|
+            \\_ ->
+                let
+                    three =
+                        sum 1 2
+                in
+                Expect.equal three 3
+        ]
+`;
+
+const template = Handlebars.compile(elmSpec);
+
+for (let i = 0; i < 5000; i++) {
+  const result = template({ i });
+  fs.writeFile(`./tests/Tests${i}.elm`, result, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });
+}
---
+++ tests/Tests960.elm
@@ -0,0 +1,18 @@
+module Tests960 exposing (..)
+
+import Expect
+import Sum exposing (sum)
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "sum"
+        [ test "1 + 2 = 3" <|
+            \_ ->
+                let
+                    three =
+                        sum 1 2
+                in
+                Expect.equal three 3
+        ]
